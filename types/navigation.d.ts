e5a5541e34a5621f039a7fcb3610969c1acec69d
unknown <ahmedatefromiah@gmail.com>
2025-09-15 22:46:08 +0300

    packaging

---
+++ types/navigation.d.ts
@@ -0,0 +1,8 @@
+// types/navigation.d.ts
+declare module '@react-navigation/native' {
+  export * from '@react-navigation/core';
+}
+
+declare module '@react-navigation/native-stack' {
+  export * from '@react-navigation/core';
+}
